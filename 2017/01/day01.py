5bff700981675ff045a606689707def575bec16a
gmnr <guido.minieri@gmail.com>
2019-12-01 12:11:50 +0100

    first commit

---
+++ 2017/01/day01.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Advent of code day 1 solution
+"""
+
+__author__ = 'Guido Minieri'
+__license__ = 'GPL'
+__version__ = 'x.x.x'
+
+#===========================================
+#           PART 1 SOLUTION
+#===========================================
+
+# import statemets
+from itertools import cycle
+
+# load the content of the file in memory
+with open('input.txt', 'r') as f:
+    data = f.read()
+
+# define the infinite list
+rolling_list = cycle(list(data))
+
+for i in range(10000):
+    print(next(rolling_list))
+
