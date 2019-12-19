b785037f1e2703bb566ae80a95d5c38056103bc9
gmnr <guido.minieri@gmail.com>
2019-12-20 00:47:22 +0100

    solution for day01 2015

---
+++ 2015/01/day01.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Solution for day01 2015
+"""
+
+__author__ = 'Guido Minieri'
+__license__ = 'GPL'
+
+
+with open("input.txt", "r") as f:
+    data = f.read().strip()
+
+solution1 = sum([+1 if x == '(' else -1 for x in data])
+
+tot = 0
+for k, v in enumerate(data):
+    tot += (-1, 1)[v == '(']
+    if tot == -1:
+        solution2 = k + 1
+        break
+
