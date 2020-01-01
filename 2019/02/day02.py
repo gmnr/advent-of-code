0e2e9e92bcdd288190e00a2fe1ec33dbcd4c89b0
gmnr <guido.minieri@gmail.com>
2020-01-01 18:28:34 +0100

    refactored solution for day2 to allow for encapsulation in Intcode class

--- 2019/02/day02.py
+++ 2019/02/day02.py
@@ -2,55 +2,24 @@
 # -*- coding: utf-8 -*-
 
 """
-Docstring
+Solution for day2 2019
 """
 
 __author__ = 'Guido Minieri'
 __license__ = 'GPL'
 
 
+import sys; sys.path.append('..')
+from intcode import Intcode
+
 # get data
 with open('input.txt', 'r') as f:
-    data = f.read().strip().split(',')
+    data = f.read()
 
 
-# Part 1
-source_opcode = [int(x) for x in data]
-
-def opcode_routine(lst, noun, verb):
-    """wrap the opcode routine into a function"""
-
-    arr = list(lst)
-    arr[1] = noun
-    arr[2] = verb
-
-    for j in range(0, len(arr), 4):
-        if arr[j] == 1:
-            arr[arr[j + 3]] = arr[arr[j + 1]] + arr[arr[j + 2]]
-        if arr[j] == 2:
-            arr[arr[j + 3]] = arr[arr[j + 1]] * arr[arr[j + 2]]
-        if arr[j] == 99:
-            break
-
-    return arr[0]
-
-solution1 = opcode_routine(source_opcode, 12, 2)
-
-
-# Part 2
-expected_result = 19690720
-
-# Loop for all permutations of 2 digits between 0-99
-for noun in range(100):
-    for verb in range(100):
-        if opcode_routine(source_opcode, noun, verb) == expected_result:
-            solution2 = 100 * noun + verb
-            break
-    else:
-        continue
-    break
+solution = Intcode(data)
 
 # Print the solutions
-print(f"The solution for Part 1 is {solution1}")
-print(f"The solution for Part 2 is {solution2}")
+print(f"The solution for Part 1 is {solution.day2()}")
+print(f"The solution for Part 2 is {solution.find_target(19690720)}")
 
--- 2019/intcode/Intcode.py
+++ 2019/intcode/Intcode.py
@@ -12,16 +12,39 @@ __license__ = 'GPL'
 class Intcode():
 
     def __init__(self, data):
-        self.data = data
+        self.data = [int(x) for x in data[:-1].strip().split(',')]
 
-    def instructions(self):
 
-        instructions = self.data.strip().split(",")
-        instructions = [int(x) for x in instructions]
+    def day2(self, noun=12, verb=2):
+        """wrap the opcode routine into a function"""
 
-        return instructions
+        self.arr = list(self.data)
+        self.arr[1] = noun
+        self.arr[2] = verb
 
-    self.data = instructions(data)
+        for j in range(0, len(self.arr), 4):
+            if self.arr[j] == 1:
+                self.arr[self.arr[j + 3]] = self.arr[self.arr[j + 1]] + self.arr[self.arr[j + 2]]
+            if self.arr[j] == 2:
+                self.arr[self.arr[j + 3]] = self.arr[self.arr[j + 1]] * self.arr[self.arr[j + 2]]
+            if self.arr[j] == 99:
+                break
+
+        return self.arr[0]
+
+    def find_target(self, target):
+        """find the noun and verb that satisfy the conditions"""
+
+        self.target = target
+
+        for noun in range(100):
+            for verb in range(100):
+                if self.day2(noun, verb) == self.target:
+                    return 100 * noun + verb
+             
+
 
     def __repr__(self):
-        return f"The len of the data is {len(self.data)}"
+        """Check head of file"""
+
+        return ", ".join([str(self.data[x]) for x in range(12)])
