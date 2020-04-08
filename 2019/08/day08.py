bc49dc0e227749c1200801c5ddccf80dd8c2713f
gmnr <guido.minieri@gmail.com>
2020-04-08 23:50:08 +0200

    solved day08 part 1

--- 2019/08/day08.py
+++ 2019/08/day08.py
@@ -2,7 +2,11 @@
 # -*- coding: utf-8 -*-
 
 """
+<<<<<<< Updated upstream
 solution for day 8
+=======
+Solutions for day08 2019
+>>>>>>> Stashed changes
 """
 
 __author__ = 'Guido Minieri'
@@ -15,6 +19,7 @@ with open('input.txt', 'r') as f:
 
 class Layer:
 
+<<<<<<< Updated upstream
     def __init__(self, arr):
         self.arr = arr
 
@@ -34,10 +39,34 @@ class Picture:
         self.layers = []
         self.width = width
         self.height = height
+=======
+    def __init__(self, source, width=25, height=6):
+        self.source = source
+        self.width = width
+        self.height = height
+
+    def findValue(self, value):
+        return len([x for x in self.source if x == value])
+
+    def productValues(self, x, y):
+        amount1 = self.findValue(x)
+        amount2 = self.findValue(y)
+        return amount1 * amount2
+
+
+class Picture(Layer):
+
+    def __init__(self, source, *args, **kwargs):
+        self.source = source
+
+        super().__init__(source, *args, **kwargs)
+        self.layers = []
+>>>>>>> Stashed changes
         self.compose()
 
 
     def compose(self):
+<<<<<<< Updated upstream
         acc = []
         for i in range(len(self.source)):
             acc.append(self.source[i])
@@ -55,3 +84,23 @@ class Picture:
 
 x = Picture(data)
 print(len(x.layers))
+=======
+
+        arr = []
+
+        for i in range(len(self.source)):
+            if i == 0:
+                arr.append(int(self.source[i]))
+                continue
+            elif i % (self.width * self.height) == 0:
+                self.layers.append(Layer(arr))
+                arr = []
+            arr.append(int(self.source[i]))
+        self.layers.append(Layer(arr))
+
+
+pic = Picture(data)
+min_pic = pic.layers[min([(i, x.findValue(0)) for i, x in enumerate(pic.layers)], key = lambda t: t[1])[0]]
+
+print(min_pic.productValues(1,2))
+>>>>>>> Stashed changes
