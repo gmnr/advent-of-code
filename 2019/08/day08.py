674f72fe803d94bc2a1ce7f20131f2705103a4f7
gmnr <guido.minieri@gmail.com>
2020-04-09 14:11:48 +0200

    solution for day08 2019 part 2

--- 2019/08/day08.py
+++ 2019/08/day08.py
@@ -9,6 +9,8 @@ __author__ = 'Guido Minieri'
 __license__ = 'GPL'
 
 
+from matplotlib import pyplot as plt
+
 with open('input.txt', 'r') as f:
     data = f.read().strip()
 
@@ -19,6 +21,7 @@ class Layer:
         self.source = source
         self.width = width
         self.height = height
+        self.out = self.__repr__()
 
     def findValue(self, value):
         return len([x for x in self.source if x == value])
@@ -28,6 +31,29 @@ class Layer:
         amount2 = self.findValue(y)
         return amount1 * amount2
 
+    def decompose(self):
+
+        res = self.out.split('\n')
+
+        res = [[int(y) for y in x] for x in res]
+        return res
+
+
+
+    def __repr__(self):
+        final = ""
+
+        for i in range(len(self.source)):
+            if i == 0:
+                final += str(self.source[i])
+                continue
+            elif i % (self.width) == 0:
+                final += '\n'
+            final += str(self.source[i])
+
+        return final
+
+
 
 class Picture(Layer):
 
@@ -36,10 +62,10 @@ class Picture(Layer):
 
         super().__init__(source, *args, **kwargs)
         self.layers = []
-        self.compose()
+        self.parse_layers()
 
 
-    def compose(self):
+    def parse_layers(self):
 
         arr = []
 
@@ -48,13 +74,44 @@ class Picture(Layer):
                 arr.append(int(self.source[i]))
                 continue
             elif i % (self.width * self.height) == 0:
-                self.layers.append(Layer(arr))
+                self.layers.append(Layer(arr, self.width, self.height))
                 arr = []
             arr.append(int(self.source[i]))
-        self.layers.append(Layer(arr))
+        self.layers.append(Layer(arr, self.width, self.height))
+
+
+    def compose(self):
+
+        image = [None] * self.width * self.height
+
+        for i in range(len(image)):
+
+            for layer in self.layers:
+                for j in range(i, len(layer.source)):
+                    if j < i:
+                        continue
+                    else:
+                        if layer.source[j] == 2:
+                            break
+                        else:
+                            if image[i] != None:
+                                break
+                            else:
+                                image[i] = layer.source[j]
+                                break
+
+            
+
+        return Layer(image, self.width, self.height)
 
 
 pic = Picture(data)
 min_pic = pic.layers[min([(i, x.findValue(0)) for i, x in enumerate(pic.layers)], key = lambda t: t[1])[0]]
+# print(min_pic.productValues(1,2))
 
-print(min_pic.productValues(1,2))
+complete_pic = pic.compose()
+print(complete_pic)
+
+message = complete_pic.decompose()
+plt.imshow(message)
+plt.show()
