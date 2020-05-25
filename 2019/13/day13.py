cb9fdfe306f51cd791d0a719462d4b580eeb3397
gmnr <guido.minieri@gmail.com>
2020-05-25 15:10:01 +0200

    completed part 13 2019

--- 2019/13/day13.py
+++ 2019/13/day13.py
@@ -22,8 +22,6 @@ class Game(Intcode):
     def __init__(self, data):
         self.out = []
         self.score = None
-        self.width = 36
-        self.height = 20
         super().__init__(data)
 
 
@@ -40,59 +38,38 @@ class Game(Intcode):
             tile = self.outputs[-1]
             self.out.append([x, y, tile])
 
-            # if tile == 4:
-                # if x > 1:
-                    # self.inpt = 1
-                # elif x < 1:
-                    # self.inpt = -1
-                # else:
-                    # self.inpt = 0
+            if tile == 4:
+                
+                try:
+                    px, py, ptile = self.findType(3)[0]
+                except:
+                    px, py, ptile = [0, 0, 0]
 
-            # if x == -1 and y == 0:  # capture the score of the game
-                # self.score = tile
+                if x > px:
+                    self.inpt = 1
+                elif x < px:
+                    self.inpt = -1
+                else:
+                    self.inpt = 0
 
-
-    # def getScore(self):
-        # return self.score
-
-
-    # def play(self):
-        # while True:
-            # self.c = 0
-            # self.parse()
-            # if self.findType(2) == 0:
-                # break
-            # self.getScore()
+            if x == -1 and y == 0:  # capture the score of the game
+                self.score = tile
 
 
     def findType(self, tileId):
-        res = [x for x in self.out if x[1] == tileId]
-        return len(res)
+        res = [x for x in self.out[::-1] if x[2] == tileId]
+        return res
+
+    
+    def numBlocks(self):
+        listBlocks = self.findType(2)
+        return len(listBlocks)
 
 
-    conv = {
-            0: " ",
-            1: "W",
-            2: "x",
-            3: "=",
-            4: "O"
-    }
-
-
-    # def __str__(self):
-        # """draws the arcade on the screen"""
-        # string = ""
-        # for line in self.width:
-            # for char in self.:
-
-
-
-
-
-        # return string
+    conv = {0: " ", 1: "W", 2: "x", 3: "=", 4: "O"}
 
 
 arcade = Game(data)
-# print(arcade.findType(2))
-print(arcade.out)
+print(arcade.numBlocks())
+print(arcade.score)
 
