e29cada43df5aab71a6ec5373868200480451ccf
gmnr <guido.minieri@gmail.com>
2020-12-02 11:04:05 +0100

    Add solution for day 02

---
+++ 2020/02/day02.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Solution for day 02 2020
+"""
+
+__author__ = 'Guido Minieri'
+__license__ = 'GPL'
+
+with open('input.txt', 'r') as f:
+    data = f.read().split('\n')[:-1]
+
+def validate_pt1(rule, pwd):
+    amount, target = rule.split(' ')
+    target = str(target)
+    min_a, max_a = amount.split('-')
+    min_a = int(min_a)
+    max_a = int(max_a)
+    pwd_a = pwd.count(target)
+    return pwd_a in range(min_a, max_a + 1)
+
+# solve part 1
+count_pt1 = 0
+for item in data:
+    if validate_pt1(*item.split(': ')):
+        count_pt1 += 1
+print(count_pt1)
+
+def validate_pt2(rule, pwd):
+    amount, target = rule.split(' ')
+    target = str(target)
+    min_a, max_a = amount.split('-')
+    min_a = int(min_a) - 1
+    max_a = int(max_a) - 1
+    pos = [pwd[min_a], pwd[max_a]]
+    return target in pos and not "".join(pos) == target * 2
+
+# solve part 2
+count_pt2 = 0
+for item in data:
+    if validate_pt2(*item.split(': ')):
+        count_pt2 += 1
+print(count_pt2)
