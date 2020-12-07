a07dfc8a013b82060e1636d2c16f5dfac2d3b051
gmnr <guido.minieri@gmail.com>
2020-12-07 01:40:11 +0100

    Solution for day 6 2017

---
+++ 2017/06/day06.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Solution for day 06 2017
+"""
+
+__author__ = 'Guido Minieri'
+__license__ = 'GPL'
+
+
+with open('input.txt', 'r') as f:
+    data = f.read()[:-1].split('\t')
+
+from itertools import cycle, islice
+
+# data = ['0', '2', '7', '0']
+banks = [int(x) for x in data]
+
+cycles = 0
+seen = []
+while True:
+    amt = max(banks)
+    start_idx = banks.index(amt)
+    banks[start_idx] = 0
+    iter_banks = islice(cycle(range(len(banks))), start_idx + 1, None)
+    for i in range(amt):
+        banks[next(iter_banks)] += 1
+
+    bank_status = "".join([str(x) for x in banks])
+    cycles += 1
+    if banks in seen:
+        part2 = banks
+        break
+    seen.append(list(banks))
+
+# pt 1
+print(cycles)
+# pt 2
+print(len(seen) - seen.index(part2))
