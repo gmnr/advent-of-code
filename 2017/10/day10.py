4efbeebd544a69b51c25f384c30b27f8e2060580
gmnr <guido.minieri@gmail.com>
2020-12-13 02:21:53 +0100

    Refactored solution for importing know hash function

--- 2017/10/day10.py
+++ 2017/10/day10.py
@@ -16,6 +16,9 @@ from collections import deque as dq
 from itertools import islice as isl
 from functools import reduce
 
+def dumb():
+    print('suck it ')
+
 string = dq([x for x in range(256)])
 c = 0
 skip = 0
@@ -31,28 +34,30 @@ for l in lenghts:
     c += l + skip
     skip += 1
 
-rope = dq([x for x in range(256)])
-c = 0
-skip = 0
-inst = [ord(x) for x in data[:-1]]
-inst += [17, 31, 73, 47, 23]
+def hash_knot(string):
+    rope = dq([x for x in range(256)])
+    c = 0
+    skip = 0
+    inst = [ord(x) for x in string]
+    inst += [17, 31, 73, 47, 23]
 
-for _ in range(64):
-    for l in inst:
-        rope.rotate(-c)
-        sel = list(isl(rope, l))
-        rest = list(isl(rope, l, len(rope)))
-        sel = sel[::-1]
-        rope = dq(sel+rest)
-        rope.rotate(c)
-        c += l + skip
-        skip += 1
+    for _ in range(64):
+        for l in inst:
+            rope.rotate(-c)
+            sel = list(isl(rope, l))
+            rest = list(isl(rope, l, len(rope)))
+            sel = sel[::-1]
+            rope = dq(sel+rest)
+            rope.rotate(c)
+            c += l + skip
+            skip += 1
+    res = list(rope)
+    chunks = [res[i:i+16] for i in range(0, len(res), 16)]
+    msg = [hex(reduce(lambda x, y: x ^ y, ch)) for ch in chunks]
+    return "".join([x[2:].zfill(2) for x in msg])
 
 # pt 1
 string = list(string)
 print(string[0] * string[1])
 # pt 2
-res = list(rope)
-chunks = [res[i:i+16] for i in range(0, len(res), 16)]
-msg = [hex(reduce(lambda x, y: x ^ y, ch)) for ch in chunks]
-print("".join([x[2:].zfill(2) for x in msg]))
+print(hash_knot(data[:-1]))
