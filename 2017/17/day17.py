1034d19ed8eaafbb3e4db60af90e15447e604206
gmnr <guido.minieri@gmail.com>
2020-12-25 01:01:22 +0100

    Solution for day17 2017

---
+++ 2017/17/day17.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Solution for day17 2017
+"""
+
+__author__ = 'Guido Minieri'
+__license__ = 'GPL'
+
+
+with open('input.txt', 'r') as f:
+    data = int(f.read().rstrip())
+
+from collections import deque
+
+def spinlock(arr, step, val):
+    arr.rotate(-step)
+    arr.insert(1, val)
+    arr.rotate(-1)
+    return arr
+
+# pt 1
+arr = deque([0])
+for i in range(1, 2018):
+    arr = spinlock(arr, data, i)
+print(arr[1])
+# pt 2
+n_arr = deque([0])
+for i in range(1, 50_000_000):
+    arr = spinlock(n_arr, data, i)
+idx = arr.index(0)
+print(arr[idx+1])
