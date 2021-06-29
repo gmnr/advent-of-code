ee7152e0ee4412ad3f17b097f1568846bdeaa578
gmnr <guido.minieri@gmail.com>
2021-06-29 10:24:31 +0200

    Remove python env for leaner fzffing

--- utils/startup.sh
+++ utils/startup.sh
@@ -6,17 +6,13 @@ DAY=$(date +"%d")
 
 # set root of project folder
 ROOT="${HOME}/Coding/advent-of-code/"
-ENV_FLD="adv-env/bin/activate"
 
 # run python eng
-source $ROOT$ENV_FLD
+source $ROOT
 
 # execute script 
 cd $ROOT"utils"
 python get_input.py
-
-# deactivate environement
-deactivate
 
 # move to folder
 cd $ROOT$YEAR"/"$DAY
