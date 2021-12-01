843f82de21d46f061f5436a920b61959f3959a4c
gmnr <guido.minieri@gmail.com>
2021-12-01 10:20:12 +0100

    Update shell script for compatibility

--- utils/startup.sh
+++ utils/startup.sh
@@ -7,12 +7,9 @@ DAY=$(date +"%d")
 # set root of project folder
 ROOT="${HOME}/Coding/advent-of-code/"
 
-# run python eng
-source $ROOT
-
 # execute script 
 cd $ROOT"utils"
-python get_input.py
+python3 get_input.py
 
 # move to folder
 cd $ROOT$YEAR"/"$DAY
