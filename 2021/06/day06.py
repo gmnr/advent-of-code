367e1ec0fedc316e174c0ae496ac80113249b302
gmnr <guido.minieri@gmail.com>
2021-12-06 10:40:16 +0100

    Solution for day06 2021

---
+++ 2021/06/day06.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Solution for day06 2021
+"""
+
+__author__ = 'Guido Minieri'
+__license__ = 'GPL'
+
+
+with open('input.txt', 'r') as f:
+    data = list(map(int, f.read().strip().split(',')))
+
+from collections import defaultdict
+
+def grow(data, days):
+    fish = defaultdict(int)
+
+    for f in data:
+        fish[f] += 1
+
+    for _ in range(days):
+        new_fish = defaultdict(int)
+        for k, v in fish.items():
+            if k == 0:
+                new_fish[8] += v
+                new_fish[6] += v
+            else:
+                new_fish[k - 1] += v
+
+            fish = new_fish
+    return fish, sum(fish.values())
+
+fish, number = grow(data, 80)
+print(number)
+fish, number = grow(data, 256)
+print(number)
