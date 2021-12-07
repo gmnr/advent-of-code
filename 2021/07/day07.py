bfc5b254dca17855836f47a66dbeeffa0907ece1
gmnr <guido.minieri@gmail.com>
2021-12-07 10:11:33 +0100

    Solution for day07 2021

---
+++ 2021/07/day07.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Solution for day07 2021
+"""
+
+__author__ = 'Guido Minieri'
+__license__ = 'GPL'
+
+
+with open('input.txt', 'r') as f:
+    crabs = list(map(int, f.read().split(',')))
+
+def align(crabs, target):
+    expenditure = 0
+    for c in crabs:
+        expenditure += abs(c - target)
+    return expenditure
+
+def triangular_num(n):
+    return (n**2 + n) // 2
+
+def align_incremental(crabs, target):
+    expenditure = 0
+    for c in crabs:
+        expenditure += triangular_num(abs(c - target))
+    return expenditure
+
+def find_min(crabs, fn):
+    return min([fn(crabs, x) for x in range(min(crabs), max(crabs) + 1)])
+
+# pt 1
+print(find_min(crabs, align))
+
+# pt 2
+print(find_min(crabs, align_incremental))
