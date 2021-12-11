1a603b87c6e91b4258fe6ce58fab83cf7dc39364
gmnr <guido.minieri@gmail.com>
2021-12-11 10:50:08 +0100

    Solution for day11 2021

---
+++ 2021/11/day11.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Solution for day11 2021
+"""
+
+__author__ = 'Guido Minieri'
+__license__ = 'GPL'
+
+
+with open('input.txt', 'r') as f:
+    data = f.read().splitlines()
+
+def parse(data):
+    octopuses = {}
+    for y, oct in enumerate(data):
+        for x, lvl in enumerate(oct):
+            octopuses[x, y] = int(lvl)
+    return octopuses
+
+def nb8(coord):
+    x, y = coord
+    return [(x + dx, y + dy) for dx, dy in [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]]
+
+def turn(data):
+    for k, v in data.items():
+        data[k] += 1
+
+    while True:
+        for k, v in data.items():
+            if v > 9:
+                nb = nb8(k)
+                for n in nb:
+                    if n not in data:
+                        continue
+                    else:
+                        data[n] += 1
+                data[k] = -9999
+
+        if all([True if x <= 9 else False for x in data.values()]):
+            break
+
+    for k, v in data.items():
+        if v < 0:
+            data[k] = 0
+
+    return data
+
+# pt 1
+oct = parse(data)
+flash = 0
+for _ in range(100):
+    oct = turn(oct)
+    flash += list(oct.values()).count(0)
+print(flash)
+
+# pt 2
+step = 101
+while True:
+    oct = turn(oct)
+    count = list(oct.values()).count(0)
+    if count == 100:
+        break
+    step += 1
+print(step)
