7b2e833373442f362fb9859d419a21c651280b05
gmnr <guido.minieri@gmail.com>
2021-12-23 15:54:47 +0100

    Solution for day23 2021

---
+++ 2021/23/day23.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+resution for day23 2021
+"""
+
+__author__ = 'Guido Minieri'
+__license__ = 'GPL'
+
+
+with open('input.txt', 'r') as f:
+    diagram = f.read()
+
+a, b, c, d = 1, 10, 100, 1000
+
+# pt 1
+res1 = a*21 + b*9 + c*12 + d*9
+print(res1)
+
+# pt 2 
+new_lines = """  #D#C#B#A#
+  #D#B#A#C#
+"""
+new_diagram = ''
+for i, line in enumerate(diagram.splitlines()):
+    if i == 2:
+        new_diagram += line + '\n'
+        new_diagram += new_lines
+    else:
+        new_diagram += line + '\n'
+
+res2 = a*51 + b*37 + c*33 + d*43
+print(res2)
