015ab0ad22d3de2944597c638ac94b183021752a
gmnr <guido.minieri@gmail.com>
2023-12-04 19:39:58 +0100

    Solution for day15 2016
    fixup

---
+++ 2016/15/day15.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Solution for day15 2016
+"""
+
+__author__ = "Guido Minieri"
+__license__ = "GPL"
+
+
+from helper import advent as aoc
+
+data = aoc.read_input()
+
+discs = []
+for x in data:
+    disc, pos, _, start = aoc.ints(x)
+    discs.append((pos, start + disc))
+
+
+def solve():
+    t = 0
+    while True:
+        res = []
+        for d in discs:
+            ratio = d[0]
+            pos = d[1] + t
+            res.append(pos % ratio)
+        if res == [0] * len(discs):
+            return t
+        t += 1
+
+
+# pt 1
+print(solve())
+
+# pt 2
+discs.append((11, 7))
+print(solve())
