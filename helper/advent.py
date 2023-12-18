4810614ed6f5a5ddad6f09dd70c45bcce9c042fb
gmnr <guido.minieri@gmail.com>
2023-12-18 21:26:02 +0100

    helper: add `astar` search

--- helper/advent.py
+++ helper/advent.py
@@ -9,6 +9,7 @@ __author__ = "Guido Minieri"
 __license__ = "GPL"
 
 import re
+from heapq import heappop, heappush
 
 
 def read_input(src="input", parser=str, sep="\n") -> tuple:
@@ -74,3 +75,26 @@ def manhattan_dist(a, b) -> int:
     ax, ay = a
     bx, by = b
     return abs(ax - bx) + abs(ay - by)
+
+
+def astar(start, h_func, moves_func):
+    "Find a shortest sequence of states from start to a goal state (a state s with h_func(s) == 0)."
+    frontier = [(h_func(start), start)]
+    previous = {start: None}
+    path_cost = {start: 0}
+    while frontier:
+        (f, s) = heappop(frontier)
+        if h_func(s) == 0:
+            return Path(previous, s)
+        for s2 in moves_func(s):
+            new_cost = path_cost[s] + 1
+            if s2 not in path_cost or new_cost < path_cost[s2]:
+                heappush(frontier, (new_cost + h_func(s2), s2))
+                path_cost[s2] = new_cost
+                previous[s2] = s
+    return dict(fail=True, front=len(frontier), prev=len(previous))
+
+
+def Path(previous, s):
+    "Return a list of states that lead to state s, according to the previous dict."
+    return [] if (s is None) else Path(previous, previous[s]) + [s]
