14df5bdd8706056f1d6184907ca51c753dc13ce4
gmnr <guido.minieri@gmail.com>
2023-12-25 10:32:28 +0100

    Solution for day25 2023

---
+++ 2023/25/day25.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Solution for day25 2023
+"""
+
+__author__ = "Guido Minieri"
+__license__ = "GPL"
+
+
+import helper.advent as aoc
+from itertools import combinations
+import networkx as nx
+
+data = aoc.read_input()
+
+graph = nx.Graph()
+
+for line in data:
+    parent, children = line.split(": ")
+    children = children.split()
+    for c in children:
+        graph.add_edge(parent, c, capacity=1)
+
+for vert1, vert2 in combinations(list(graph.nodes), 2):
+    nodes_removed, resulting_graphs = nx.minimum_cut(graph, vert1, vert2)
+    if nodes_removed == 3:
+        graph1, graph2 = resulting_graphs[0], resulting_graphs[1]
+        print(len(graph1) * len(graph2))
+        break
