49956ae648dd7a526fbf2258ef67c6877e32c972
gmnr <guido.minieri@gmail.com>
2024-12-03 15:16:27 +0100

    Solution for day19 2016

---
+++ 2016/19/day19.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Solution for day19 2016
+"""
+
+__author__ = "Guido Minieri"
+__license__ = "GPL"
+
+
+from helper import advent as aoc
+from collections import deque
+
+data = aoc.first(aoc.read_input(parser=int))
+
+# pt 1
+elves = deque([x for x in range(1, data + 1)])
+while len(elves) > 1:
+    elves.rotate(-1)
+    elves.popleft()
+print(aoc.first(elves))
+
+# pt 2
+elves = deque([x for x in range(1, data + 1)])
+opposite = len(elves) // 2
+elves.rotate(-opposite)
+while len(elves) > 1:
+    elves.popleft()
+    old = opposite
+    opposite = len(elves) // 2
+    rotate_amount = old - opposite
+    elves.rotate(rotate_amount - 1)
+print(aoc.first(elves))
