5fbec59da401aa3e56bf1c16f8665961f9c0bb78
gmnr <guido.minieri@gmail.com>
2024-12-06 13:42:09 +0100

    Solution for day06 2024

---
+++ 2024/06/day06.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Solution for day06 2024
+"""
+
+__author__ = "Guido Minieri"
+__license__ = "GPL"
+
+
+import helper.advent as aoc
+from itertools import cycle
+
+data = aoc.read_input()
+
+coord = {}
+guard = (0, 0)
+for y, line in enumerate(data):
+    for x, c in enumerate(line):
+        coord[(x, y)] = c
+        if c == "^":
+            guard = (x, y)
+
+
+def patrol(pos, coord):
+    visited = {pos: None}
+
+    DIRS = {"^": (0, -1), ">": (1, 0), "<": (-1, 0), "v": (0, 1)}
+    turns = cycle(["^", ">", "v", "<"])
+
+    for d in turns:
+
+        new_pos = tuple(map(sum, zip(pos, DIRS[d])))
+
+        while coord[new_pos] != "#":
+
+            visited[new_pos] = None
+            pos = new_pos
+            new_pos = tuple(map(sum, zip(pos, DIRS[d])))
+
+            if new_pos not in coord:
+                return visited
+    return visited
+
+
+def looped(pos, coord):
+    visited = {(pos, "^"): None}
+
+    DIRS = {"^": (0, -1), ">": (1, 0), "<": (-1, 0), "v": (0, 1)}
+    turns = cycle(["^", ">", "v", "<"])
+
+    for d in turns:
+
+        new_pos = tuple(map(sum, zip(pos, DIRS[d])))
+
+        if (new_pos, d) in visited.keys():
+            return True
+
+        while coord[new_pos] != "#":
+
+            visited[(new_pos, d)] = None
+            pos = new_pos
+            new_pos = tuple(map(sum, zip(pos, DIRS[d])))
+
+            if new_pos not in coord:
+                return False
+
+
+# pt 1
+visited = patrol(guard, coord)
+print(len(visited.keys()))
+
+# pt 2
+loops = 0
+for i, p in enumerate(visited.keys()):
+    if i == 0:
+        continue
+
+    trial = dict(coord)
+    trial[p] = "#"
+
+    if looped(guard, trial):
+        loops += 1
+print(loops)
