fbcf59385d69c8553a9ea2f19506dd1a5cc41f34
gmnr <guido.minieri@gmail.com>
2025-12-01 18:04:28 +0100

    update `startup.sh` for 2025 edition

--- utils/startup.sh
+++ utils/startup.sh
@@ -3,9 +3,10 @@
 # inizialize date and day
 YEAR=$(date +"%Y")
 DAY=$(date +"%d")
+URL_DAY=$(date +"%-d")
 
 # set root of project folder
-ROOT="${HOME}/Coding/advent-of-code/"
+ROOT="${HOME}/Documents/advent-of-code/"
 
 # execute script 
 cd $ROOT"utils"
@@ -15,7 +16,7 @@ python3 get_input.py
 cd $ROOT
 if cd $YEAR ; then
   cd $DAY
-  open "https://adventofcode.com/$YEAR/day/$DAY"
+  open "https://adventofcode.com/$YEAR/day/$URL_DAY"
   nvim "day$DAY.py"
 else
   nvim
