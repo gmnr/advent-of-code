1dffa30857c4b80658a631b78214d1b6685dcdb3
gmnr <guido.minieri@gmail.com>
2025-12-04 08:21:40 +0100

    Solution for day04 2025

---
+++ 2025/04/day04.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Solution for day04 2025
+"""
+
+__author__ = "Guido Minieri"
+__license__ = "GPL"
+
+
+import helper.advent as aoc
+from collections import Counter
+
+data = aoc.read_input()
+
+grid = {}
+for y, line in enumerate(data):
+    for x, char in enumerate(line):
+        grid[(x, y)] = char
+
+to_remove = []
+
+# pt 1
+movable = 0
+for c in grid:
+    if grid[c] != "@":
+        continue
+    adjacent = aoc.gen_coordinates(c, n=8)
+    vals = []
+    for x in adjacent:
+        if x not in grid:
+            continue
+        vals.append(grid[x])
+
+    vals = Counter(vals)
+    if vals["@"] < 4:
+        movable += 1
+        to_remove.append(c)
+print(movable)
+
+# pt 2
+removed = 0
+while to_remove:
+    for x in to_remove:
+        grid[x] = "."
+        removed += 1
+        to_remove.pop()
+    to_remove = []
+    for c in grid:
+        if grid[c] != "@":
+            continue
+        adjacent = aoc.gen_coordinates(c, n=8)
+        vals = []
+        for x in adjacent:
+            if x not in grid:
+                continue
+            vals.append(grid[x])
+
+        vals = Counter(vals)
+        if vals["@"] < 4:
+            to_remove.append(c)
+print(removed)
