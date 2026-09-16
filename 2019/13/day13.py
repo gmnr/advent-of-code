08d025e31f1fc14e455f8d471b8021f60f9531c1
gmnr <guido.minieri@gmail.com>
2026-09-16 10:42:53 +0200

    Intcode: refactor day 13

--- 2019/13/day13.py
+++ 2019/13/day13.py
@@ -9,6 +9,7 @@ __author__ = "gmnr"
 __license__ = "GPL"
 
 
+from collections import defaultdict
 import sys
 
 sys.path.append("..")
@@ -17,53 +18,56 @@ from intcode import Intcode
 with open("input.txt", "r") as f:
     data = f.read()
 
+# pt1
+game = defaultdict(list)
+vm = Intcode(data)
+process = vm.run()
 
-class Game(Intcode):
-    def __init__(self, data):
-        self.out = []
-        self.score = None
-        self.paddle = []
-        super().__init__(data)
+try:
+    while not vm.halted:
+        x = next(process)
+        y = next(process)
+        id = next(process)
 
-    def move_ball(self):
-        if len(self.outputs) % 3 == 0 and self.outputs != []:
-            x = self.outputs[-3]
-            y = self.outputs[-2]
-            tile = self.outputs[-1]
-            self.out.append([x, y, tile])
+        game[id].append((x, y))
 
-            if tile == 3:
-                self.paddle.append([x, y, tile])
+except StopIteration:
+    pass
 
-            if tile == 4:
-                try:
-                    px, py, ptile = self.paddle[-1]
-                except:
-                    px, py, ptile = [0, 0, 0]
+print(len(game[2]))
 
-                if x > px:
-                    self.inpt = 1
-                elif x < px:
-                    self.inpt = -1
-                else:
-                    self.inpt = 0
+# pt2
+vm = Intcode("2" + data[1:])
+process = vm.run()
 
-            if x == -1 and y == 0:  # capture the score of the game
-                self.score = tile
+score = 0
+paddle_x = 0
+ball_x = 0
 
-    def findType(self, tileId):
-        res = [x for x in self.out[::-1] if x[2] == tileId]
-        return res
+try:
+    val = next(process)
 
-    def numBlocks(self):
-        listBlocks = self.findType(2)
-        return len(listBlocks)
+    while not vm.halted:
 
+        if val == Intcode.WAITING:
+            joystick = (ball_x > paddle_x) - (ball_x < paddle_x)
+            val = process.send(joystick)
+            continue
 
-# pt 1
-arcade = Game(data)
-print(arcade.numBlocks())
+        x = val
+        y = next(process)
+        tile = next(process)
 
-# pt 2
-free_game = Game("2" + data[1:])
-print(free_game.score)
+        if x == -1 and y == 0:
+            score = tile
+        elif tile == 3:
+            paddle_x = x
+        elif tile == 4:
+            ball_x = x
+
+        val = next(process)
+
+except StopIteration:
+    pass
+
+print(score)
